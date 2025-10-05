87104e7ec7ef8770f56196962438c1c4d6aa0677
Gayan Prasanna <gayanprasanna@example.com>
2025-10-05 15:05:03 +0530

    Auto-generated commit

--- projects/angular-tree/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of angular-tree
- */
-
-export * from './lib/angular-tree';
-export * from './lib/components/tree-node.component';
-export * from './lib/services/tree.service';
-export * from './lib/models/tree-node.interface';
